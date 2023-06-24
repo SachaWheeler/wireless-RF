7844096a8e3041d7ba14962b98048c9cce1b7c44
Sacha Wheeler <sacha@sachawheeler.com>
2023-06-24 10:36:57 +0100

    flame detection and radio signal

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -11,11 +11,24 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(7, 8);  // CE, CSN
 
 const byte address[6] = "00001";
 
+//int led = 13; // define the LED pin
+int digitalPin = 2;  // KY-026 digital interface
+int analogPin = A0;  // KY-026 analog interface
+int digitalVal;      // digital readings
+int analogVal;       //analog readings
+
+const char flame[] = "flame";
+const char none[] = "none";
+
 void setup() {
+  Serial.begin(9600);
+
+  pinMode(digitalPin, INPUT);
+
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
@@ -23,7 +36,19 @@ void setup() {
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+
+  digitalVal = digitalRead(digitalPin);
+  if (digitalVal == HIGH)  // if flame is detected
+  {
+    Serial.println(flame);  // print analog value to serial
+    radio.write(&flame, sizeof(flame));
+  } else {
+    Serial.println(none);  // print analog value to serial
+    radio.write(&none, sizeof(none));
+  }
+  // Read the analog interface
+  analogVal = analogRead(analogPin);
+  Serial.println(analogVal);  // print analog value to serial
+
+  delay(4000);
 }
